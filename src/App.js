e7f3717eccb83f1b40910dcd8afb77a93536f05b
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-10-08 00:03:00 +0500

    add gitHub search

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import { Profile } from './pages/Profile'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import { Alert } from './components/Alert'
 import {AlertState} from  './context/alert/alertState'
+import { GithubState } from './context/github/GithubState'
 
 
 function App() {
   return (
-    <AlertState>
-    <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4">
-        <Alert alert={{text: 'Test alert'}}/>
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/profile/:name" component={Profile}/>
-         </Switch>
-      </div>
-    </BrowserRouter>
-    </AlertState>
+    <GithubState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert alert={{text: 'Test alert'}}/>
+            <Switch>
+              <Route path="/" exact component={Home}/>
+              <Route path="/about" component={About}/>
+              <Route path="/profile/:name" component={Profile}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </GithubState>
   );
 }
 
