5b464d147c1751c3ab51c9ef7216ea68ea8ec54c
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-10-07 22:57:31 +0500

    add alerts

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Profile } from './pages/Profile'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { Alert } from './components/Alert'
+import {AlertState} from  './context/alert/alertState'
 
 
 function App() {
   return (
+    <AlertState>
     <BrowserRouter>
       <Navbar />
       <div className="container pt-4">
+        <Alert alert={{text: 'Test alert'}}/>
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/about" component={About}/>
@@ -18,6 +22,7 @@ function App() {
          </Switch>
       </div>
     </BrowserRouter>
+    </AlertState>
   );
 }
 
